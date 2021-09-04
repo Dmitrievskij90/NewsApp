c09c9ac1a91573f3ca95c7e1f1d8ba5766c19cf5
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-04 19:36:55 +0600

    create SearchCell

---
+++ NewsApp/Views/Search/SearchCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchCell.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 04.09.2021.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+
+    static let identifier = "SearchCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
