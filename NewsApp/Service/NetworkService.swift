06b02e9f2903530c26d15eb9f1d53b537a839e20
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-05 23:11:49 +0600

    add fetchNews func to NetworkService

--- NewsApp/Service/NetworkService.swift
+++ NewsApp/Service/NetworkService.swift
@@ -10,6 +10,13 @@ import Foundation
 class NetworkService {
     static let shared = NetworkService()
 
+    func fetchNews(searchTerm: String, completion: @escaping (NewsData?, Error?) -> ()) {
+
+        let urlString = "https://newsapi.org/v2/everything?q=\(searchTerm)&sortBy=publishedAt&apiKey=61bba430f9444209af20b7856ae3d12e"
+
+        fetchData(with: urlString, completion: completion)
+    }
+
     func fetchData<T: Codable>(with urlString: String, completion: @escaping (T?, Error?) -> ()) {
                                                     
         guard let url = URL(string: urlString) else {
