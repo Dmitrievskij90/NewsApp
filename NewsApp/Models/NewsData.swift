eae57490ba3de8d12d0e49d321659514686a9de6
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-09 00:40:10 +0600

    add new parameters to NewsData

--- NewsApp/Models/NewsData.swift
+++ NewsApp/Models/NewsData.swift
@@ -13,9 +13,11 @@ struct NewsData: Codable {
 
 struct Articles: Codable {
     var author: String?
-    let title: String
     var urlToImage: String?
+    let title: String
     let content: String
+    let description: String
+    let publishedAt: String
     let source: Source
 }
 
