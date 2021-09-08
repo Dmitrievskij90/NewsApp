4ffb89aa60162c8469d11b5263bac80d501b8bf3
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-09 00:43:14 +0600

    add UI elements to NewsDetailCell

--- NewsApp/Views/Details/NewsDetailCell.swift
+++ NewsApp/Views/Details/NewsDetailCell.swift
@@ -10,9 +10,58 @@ import UIKit
 class NewsDetailCell: UICollectionViewCell {
     static let identifier = "NewsDetailCell"
 
+    var dataSource: Articles? {
+        didSet {
+            if let source = dataSource {
+                titleLabel.text = source.title
+                descriptionTextView.text = source.description
+                dateLabel.text = Helpers.shared.convertDate(date: source.publishedAt) 
+            }
+        }
+    }
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.constrainHeight(constant: 150)
+        label.font = .boldSystemFont(ofSize: 25)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.constrainHeight(constant: 40)
+        label.constrainWidth(constant: 100)
+        label.centerXInSuperview()
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .lightGray
+        label.textAlignment = .center
+        return label
+    }()
+
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.font = .systemFont(ofSize: 20)
+        textView.textColor = .darkGray
+        textView.textAlignment = .justified
+        return textView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .yellow
+
+        let verticalStackView = UIStackView(arrangedSubviews: [
+            titleLabel,
+            dateLabel,
+            descriptionTextView
+        ])
+        verticalStackView.axis = .vertical
+
+        addSubview(verticalStackView)
+        verticalStackView.fillSuperview(padding: .init(top: 0, left: 15, bottom: 0, right: 15))
     }
 
     required init?(coder: NSCoder) {
