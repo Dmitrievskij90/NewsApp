e50cd3ddab3cea9e827d4f4743ce26e74e0ed8ba
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-11 20:24:39 +0600

    create TodayCell

---
+++ NewsApp/Views/TodayCell/TodayCell.swift
@@ -0,0 +1,19 @@
+//
+//  TodayCell.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 11.09.2021.
+//
+
+import UIKit
+
+class TodayCell: BaseCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
