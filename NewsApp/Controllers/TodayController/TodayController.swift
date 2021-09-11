1f63665905860a48565d7ed3770ea7fe910043ff
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-12 01:16:23 +0600

    use fetchTodayNews method in TodayCOntroller

--- NewsApp/Controllers/TodayController/TodayController.swift
+++ NewsApp/Controllers/TodayController/TodayController.swift
@@ -10,9 +10,24 @@ import UIKit
 class TodayController: UIViewController {
 
     private var todayCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
+    private var results = [Articles]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchTodayNews()
+    }
+
+    private func fetchTodayNews() {
+        NetworkService.shared.fetchTodayNews { (results, error) in
+            if let err = error {
+                print("Can't fetch today news", err)
+            }
+            self.results = results?.articles ?? []
+            print(self.results.count)
+            DispatchQueue.main.async {
+                self.todayCollectionView.reloadData()
+            }
+        }
     }
 
     override func loadView() {
@@ -38,13 +53,17 @@ class TodayController: UIViewController {
 
 extension TodayController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return results.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodayCell.identifier, for: indexPath) as? TodayCell else {
             return UICollectionViewCell()
         }
+
+        let res = results[indexPath.item]
+
+        cell.results = res
         return cell
     }
 
