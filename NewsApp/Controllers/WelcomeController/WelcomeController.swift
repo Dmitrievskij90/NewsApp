5a13eb9818ad1a810756ff5578ecf442f6cad7e2
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-12 20:08:52 +0600

    create WelcomeController

---
+++ NewsApp/Controllers/WelcomeController/WelcomeController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeController.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 12.09.2021.
+//
+
+import UIKit
+
+class WelcomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
