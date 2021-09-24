5a556fffca540124b3f3df78bb443ecd017c6c5d
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-24 16:53:59 +0600

    create tableView in ProfileViewController
    registration of different cells and a header

--- NewsApp/Controllers/Profile Screen/ProfileVIewController.swift
+++ NewsApp/Controllers/Profile Screen/ProfileVIewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 import KeychainAccess
 
-class ProfileVIewController: UIViewController {
+class ProfileViewController: UIViewController {
     private let keychain = Keychain()
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.register(CountryCell.self, forCellReuseIdentifier: CountryCell.identifier)
+        tableView.register(ChooseCountryCell.self, forCellReuseIdentifier: ChooseCountryCell.identifier)
+        tableView.register(ProfileTableHeader.self, forHeaderFooterViewReuseIdentifier: ProfileTableHeader.identifier)
+        tableView.backgroundColor = .white
+        return tableView
+    }()
 
-    let exitButton: UIButton = {
+    private var sections = [CountrySection]()
+
+    private let exitButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.setTitle("Done", for: .normal)
+        button.setTitle("LOG OUT", for: .normal)
         button.contentMode = .center
         button.backgroundColor = .init(hex: 0xBE1FBB)
         button.layer.cornerRadius = 16
@@ -25,16 +36,26 @@ class ProfileVIewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        sections = [
+            CountrySection(title: "Choose country",
+                     countries: ["USA", "RUSSIA", "FRANCE"],
+                     countriesImages: ["usa_image", "russia_image", "france_image"])
+        ]
     }
 
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
-        view.backgroundColor = .white
         self.view = view
+        view.backgroundColor = .gray
 
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        tableView.fillSuperview(padding: .init(top: 0, left: 0, bottom: 100, right: 0))
+        
         view.addSubview(exitButton)
-        exitButton.centerInSuperview(size: .init(width: 100, height: 100), constantY: 100)
+        exitButton.anchor(top: nil, leading: nil, bottom: view.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 0, bottom: 100, right: 0), size: .init(width: 100, height: 50))
+        exitButton.centerXInSuperview()
     }
 
     @objc func exitButonPressed() {
@@ -43,5 +64,75 @@ class ProfileVIewController: UIViewController {
         let destinationVC = WelcomeController()
         destinationVC.modalPresentationStyle = .fullScreen
         present(destinationVC, animated: true, completion: nil)
+    }
+}
+
+
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = sections[section]
+        if section.isOpened {
+            return section.countries.count + 1
+        } else {
+            return 1
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ChooseCountryCell.identifier, for: indexPath) as? ChooseCountryCell else {
+                return UITableViewCell()
+            }
+            return cell
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryCell.identifier, for: indexPath) as? CountryCell else {
+            return UITableViewCell()
+        }
+        cell.countryLabel.text = sections[indexPath.section].countries[indexPath.row - 1]
+        cell.countryImageView.image = UIImage(named: sections[indexPath.section].countriesImages[indexPath.row - 1])
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.row == 0 {
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        } else {
+            let country = sections[indexPath.section].countries[indexPath.row - 1]
+            switch country {
+            case "USA":
+                UserDefaults.standard.setValue("us", forKey: "chosenCountry")
+            case "RUSSIA":
+                UserDefaults.standard.setValue("ru", forKey: "chosenCountry")
+            case "FRANCE":
+                UserDefaults.standard.setValue("fr", forKey: "chosenCountry")
+            default:
+                UserDefaults.standard.setValue("us", forKey: "chosenCountry")
+            }
+            sections[indexPath.section].isOpened = false
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: ProfileTableHeader.identifier) as? ProfileTableHeader else {
+            return UIView()
+        }
+        return header
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 200
     }
 }
