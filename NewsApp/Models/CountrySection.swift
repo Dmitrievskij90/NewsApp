bc3e0ebe13a2263ece5aa40e5b17f220080f3d0c
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-09-24 16:43:45 +0600

    create CountrySection model
    for create extendable tableView

---
+++ NewsApp/Models/CountrySection.swift
@@ -0,0 +1,15 @@
+//
+//  CountrySection.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 21.09.2021.
+//
+
+import Foundation
+
+struct CountrySection {
+    let title: String
+    let countries: [String]
+    let countriesImages: [String]
+    var isOpened = false
+}
