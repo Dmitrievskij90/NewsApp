6b3bea1672ac23e365cc407c0107cdc2557b4f88
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-10-02 16:43:16 +0600

    save categories by tap

--- NewsApp/Controllers/Profile Screen/CategoriesViewController.swift
+++ NewsApp/Controllers/Profile Screen/CategoriesViewController.swift
@@ -7,19 +7,51 @@
 
 import UIKit
 
+
 class CategoriesViewController: UIViewController {
     private var categoryCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
     private var categoriesSet:Set = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
     private var categoriesArray = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
 
+    var documentDirectorypath = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+
+    private let defaults = UserDefaults.standard
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print(categoriesSet)
+
+        var folderPath = documentDirectorypath
+        folderPath?.appendPathComponent("New test folder")
+
+        guard let pass = folderPath else {
+            return
+        }
+        print(pass)
+
+        let notFirsAppLaunch = defaults.bool(forKey: "isrue")
+        print(notFirsAppLaunch)
+
+        if !notFirsAppLaunch {
+            try? FileManager.default.createDirectory(at: pass, withIntermediateDirectories: false, attributes: nil)
+            let data = try? JSONEncoder().encode(categoriesSet)
+            let dataPath = folderPath?.appendingPathComponent("categoriesSet.json")
+            FileManager.default.createFile(atPath: dataPath!.path, contents: data, attributes: nil)
+            defaults.setValue(true, forKey: "isrue")
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tabBarController?.tabBar.isHidden = true
+
+        var folderPath = documentDirectorypath
+        folderPath?.appendPathComponent("New test folder")
+        let dataPath = folderPath?.appendingPathComponent("categoriesSet.json")
+        if let newData = FileManager.default.contents(atPath: dataPath!.path) {
+            categoriesSet = try! JSONDecoder().decode(Set<String>.self, from: newData)
+            print(categoriesSet)
+        }
     }
 
     override func loadView() {
@@ -43,6 +75,8 @@ class CategoriesViewController: UIViewController {
 
         view.addSubview(categoryCollectionView)
     }
+
+    var tr = true
 }
 
 extension CategoriesViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
@@ -69,15 +103,56 @@ extension CategoriesViewController: UICollectionViewDataSource, UICollectionView
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
+        if let cell = collectionView.cellForItem(at: indexPath),  tr {
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) {
+                cell.transform = .init(scaleX: 0.9, y: 0.9)
+//                    self.backgroundColor = .purple
+                cell.backgroundColor = .green
+//                celllayer.shadowColor = UIColor.purple.cgColor
+            }
+//            cell.backgroundColor = .green
+            tr = false
+        } else if let cell = collectionView.cellForItem(at: indexPath),  !tr {
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) {
+                cell.transform = .identity
+//                    self.backgroundColor = .purple
+                cell.backgroundColor = .white
+//                celllayer.shadowColor = UIColor.purple.cgColor
+            }
+            tr = true
+        }
+
         let item = categoriesArray[indexPath.item]
 
         if categoriesSet.contains(item) {
             categoriesSet.remove(item)
+            var folderPath = documentDirectorypath
+            folderPath?.appendPathComponent("New test folder")
+
+            guard let pass = folderPath else {
+                return
+            }
+            print(pass)
+
+            try? FileManager.default.createDirectory(at: pass, withIntermediateDirectories: false, attributes: nil)
+            let data = try? JSONEncoder().encode(categoriesSet)
+            let dataPath = folderPath?.appendingPathComponent("categoriesSet.json")
+            FileManager.default.createFile(atPath: dataPath!.path, contents: data, attributes: nil)
         } else {
             categoriesSet.insert(item)
+            var folderPath = documentDirectorypath
+            folderPath?.appendPathComponent("New test folder")
+            guard let pass = folderPath else {
+                return
+            }
+            print(pass)
+
+            try? FileManager.default.createDirectory(at: pass, withIntermediateDirectories: false, attributes: nil)
+            let data = try? JSONEncoder().encode(categoriesSet)
+            let dataPath = folderPath?.appendingPathComponent("categoriesSet.json")
+            FileManager.default.createFile(atPath: dataPath!.path, contents: data, attributes: nil)
         }
 
-//        print(categoriesSet)
-        categoryCollectionView.reloadData()
+//        categoryCollectionView.reloadData()
     }
 }
