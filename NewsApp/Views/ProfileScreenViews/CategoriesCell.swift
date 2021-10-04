67008125e2606050eeddc82cc2504148d55f218d
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-10-04 21:42:35 +0600

    add starImageView to CategoriesCell

--- NewsApp/Views/ProfileScreenViews/CategoriesCell.swift
+++ NewsApp/Views/ProfileScreenViews/CategoriesCell.swift
@@ -10,7 +10,6 @@ import UIKit
 class CategoriesCell: BaseCell {
 
     static let identifier = "CategoriesCell"
-
     let categoryLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16)
@@ -19,14 +18,30 @@ class CategoriesCell: BaseCell {
         return label
     }()
 
+    let starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "star.fill")
+        imageView.tintColor = .red
+        imageView.contentMode = .scaleAspectFit
+        imageView.constrainWidth(constant: 50)
+        return imageView
+    }()
+
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         layer.cornerRadius = 16
         backgroundColor = .white
 
-        addSubview(categoryLabel)
-        categoryLabel.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+        let stackVIew = UIStackView(arrangedSubviews: [
+            categoryLabel,
+            starImageView
+        ])
+        stackVIew.spacing = 16
+        stackVIew.alignment = .center
+
+        addSubview(stackVIew)
+        stackVIew.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
 
     }
 
