6e3c85674b7b0667bd03b2c1772c3e5f03682c97
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-10-13 23:00:44 +0600

    register categoryCollectionView in FullScreenController

--- NewsApp/Controllers/FavoriteCategories Screen/FullScreenController.swift
+++ NewsApp/Controllers/FavoriteCategories Screen/FullScreenController.swift
@@ -8,12 +8,8 @@
 import UIKit
 
 class FullScreenController: UIViewController {
-
-    var dismissHandler: (() ->())?
-    var dataSourse: String?
-    private var results = [Articles]()
-
-    let closeButton: UIButton = {
+    private var categoryCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
+    private let closeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
         button.tintColor = .init(hex: 0xBE1FBB)
@@ -21,121 +17,78 @@ class FullScreenController: UIViewController {
         return button
     }()
 
-    let tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
-        tableView.allowsSelection = false
-        tableView.showsVerticalScrollIndicator = false
-        tableView.showsHorizontalScrollIndicator = false
-        tableView.separatorStyle = .singleLine
-        tableView.isUserInteractionEnabled = true
-        tableView.register(AppFullscreenHeaderCell.self, forCellReuseIdentifier: AppFullscreenHeaderCell.identifier)
-        tableView.register(NewsCategoryCell.self, forCellReuseIdentifier: NewsCategoryCell.identifier)
-        return tableView
-    }()
-
-    private let activityIndicator: UIActivityIndicatorView = {
-        let aiv = UIActivityIndicatorView(style: .medium)
-        aiv.color = .darkGray
-        aiv.startAnimating()
-        aiv.hidesWhenStopped = true
-        return aiv
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.clipsToBounds = true
-        fetchTodayNews()
     }
 
     override func loadView() {
         let view = UIView(frame: UIScreen.main.bounds)
         self.view = view
         view.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
 
-        setupTableView()
+        setupCollectinView()
         setupCloseButton()
-
-//        view.addSubview(activityIndicator)
-//        activityIndicator.centerInSuperview()
     }
 
-    private func setupTableView() {
-        view.addSubview(tableView)
-        let height = UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect.zero
-        tableView.fillSuperview()
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: height.height, right: 0)
-        tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+    private func setupCollectinView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+
+        categoryCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        categoryCollectionView.register(NewsCategoryCell.self, forCellWithReuseIdentifier: NewsCategoryCell.identifier)
+        categoryCollectionView.backgroundColor = UIColor.white
+        categoryCollectionView.dataSource = self
+        categoryCollectionView.delegate = self
+
+        view.addSubview(categoryCollectionView)
     }
 
-    private func fetchTodayNews() {
-        let dispatchGroup = DispatchGroup()
-
-        let country = UserDefaults.standard.value(forKey: "chosenCountry") as? String ?? "us"
-
-        dispatchGroup.enter()
-        NetworkService.shared.fetchCategoriesNews(preferredCountry: country, preferredCategoty: dataSourse ?? "sports") { (results, error) in
-            if let err = error {
-                print("Can't fetch today news", err)
-            }
-            self.results = results?.articles ?? []
-            dispatchGroup.leave()
-        }
-
-        dispatchGroup.notify(queue: .main) {
-            self.activityIndicator.stopAnimating()
-            self.tableView.reloadData()
-        }
+    @objc func handleDismiss() {
+        dismiss(animated: true, completion: nil)
     }
 
     private func setupCloseButton() {
         view.addSubview(closeButton)
-        closeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 80, height: 40))
+        closeButton.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 20, left: 0, bottom: 0, right: 16), size: .init(width: 44, height: 44))
     }
 
-    @objc func handleDismiss(button: UIButton) {
-        button.isHidden = true
-        dismissHandler?()
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
+
 }
 
-extension FullScreenController: UITableViewDelegate, UITableViewDataSource {
+extension FullScreenController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: AppFullscreenHeaderCell.identifier, for: indexPath) as? AppFullscreenHeaderCell else {
-                return UITableViewCell()
-            }
-            cell.favoriteCell.dataSourse = dataSourse
-            cell.favoriteCell.layer.cornerRadius = 0
-            cell.clipsToBounds = true
-            cell.backgroundView = nil
-            return cell
-        }
-
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsCategoryCell.identifier, for: indexPath) as? NewsCategoryCell else {
-            return UITableViewCell()
-        }
-
-        let res = results[indexPath.row]
-
-        cell.results = res
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 300
-        }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 50
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsCategoryCell.identifier, for: indexPath) as? NewsCategoryCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height: CGFloat = 68
+        return .init(width: view.frame.width - 48, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return .init(top: 12, left: 24, bottom: 12, right: 24)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+
+     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let appId = self.apps[indexPath.item].id
+//        let appDetailController = AppDetailController(appId: appId)
+//        navigationController?.pushViewController(appDetailController, animated: true)
     }
 }
