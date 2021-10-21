80ca5381343a9dcd657ab38be8a11f34588fe1df
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-10-21 22:10:12 +0600

    create StockHeaderCell

---
+++ NewsApp/Views/TodayCell/StockHeaderCell.swift
@@ -0,0 +1,22 @@
+//
+//  StockHeaderCell.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 21.10.2021.
+//
+
+import UIKit
+
+class StockHeaderCell: UICollectionViewCell {
+    static let identifier = "StockHeaderCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+
+
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
