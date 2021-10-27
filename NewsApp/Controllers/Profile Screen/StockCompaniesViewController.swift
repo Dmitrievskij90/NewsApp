553cb0e407cb614dc58764b3015100e325cbee35
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-10-27 21:35:21 +0600

    create StockCompaniesViewController

---
+++ NewsApp/Controllers/Profile Screen/StockCompaniesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StockCompaniesViewController.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 27.10.2021.
+//
+
+import UIKit
+
+class StockCompaniesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
