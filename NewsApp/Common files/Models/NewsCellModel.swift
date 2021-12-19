fe68ad8afbe2323c00470b4c28246bbc8defb2f5
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-12-19 21:09:56 +0600

    create NewsCellModel

---
+++ NewsApp/Common files/Models/NewsCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCellModel.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 16.12.2021.
+//
+
+import Foundation
+
+struct NewsCellModel {
+    let author: String
+    let title: String
+    let image: String
+}
