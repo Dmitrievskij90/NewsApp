2c41218c9916b45d42286e42afa08cae35da2bca
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-01-20 21:40:22 +0600

    add init for urlString in WebNewsViewController

--- NewsApp/Screens/WebNews Screen/View/WebNewsViewController.swift
+++ NewsApp/Screens/WebNews Screen/View/WebNewsViewController.swift
@@ -10,17 +10,21 @@ import WebKit
 
 class WebNewsViewController: UIViewController, WKNavigationDelegate {
     private var webView = WKWebView()
-    var urlString = ""
+    private var urlString: String
+
+    init(urlString: String) {
+        self.urlString = urlString
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(false, animated: false)
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        let requesr = URLRequest(url: url)
-        webView.load(requesr)
-        webView.navigationDelegate = self
+        loadNews()
     }
 
     override func loadView() {
@@ -30,5 +34,14 @@ class WebNewsViewController: UIViewController, WKNavigationDelegate {
         navigationController?.navigationBar.tintColor = .label
         view.addSubview(webView)
         webView.fillSuperview()
+    }
+
+    private func loadNews() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let requesr = URLRequest(url: url)
+        webView.load(requesr)
+        webView.navigationDelegate = self
     }
 }
