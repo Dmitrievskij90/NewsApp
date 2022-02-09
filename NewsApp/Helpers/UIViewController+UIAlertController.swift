9887a148fa0dc82e0a0af483100b5be8a1b25c38
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-02-09 22:54:34 +0600

    delete presentTwoButtonAlert method from UIViewController extension

--- NewsApp/Helpers/UIViewController+UIAlertController.swift
+++ NewsApp/Helpers/UIViewController+UIAlertController.swift
@@ -9,25 +9,11 @@ import UIKit
 
 extension UIViewController {
     func presentOneButtonAlert(withTitle title: String, message: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { _ in
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        let OKAction = UIAlertAction(title: "OK", style: .destructive) { _ in
             alertController.dismiss(animated: true, completion: nil)
         }
         alertController.addAction(OKAction)
-        self.present(alertController, animated: true, completion: nil)
-    }
-
-    func presentTwoButtonAlert(withTitle title: String, message: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { _ in
-            alertController.dismiss(animated: true, completion: nil)
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in
-            alertController.dismiss(animated: true, completion: nil)
-        }
-        alertController.addAction(OKAction)
-        alertController.addAction(cancelAction)
         self.present(alertController, animated: true, completion: nil)
     }
 }
