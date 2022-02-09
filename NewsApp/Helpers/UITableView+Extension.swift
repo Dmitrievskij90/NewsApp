d0bb0c02a0b97992c4a24cd739cb3a541e479a0b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-02-09 22:45:49 +0600

    add UITableView extension

---
+++ NewsApp/Helpers/UITableView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UITableView+Extension.swift
+//  NewsApp
+//
+//  Created by Konstantin Dmitrievskiy on 09.02.2022.
+//
+
+import UIKit
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>(withClass name: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: String(describing: name), for: indexPath) as? T else {
+            fatalError(
+                "Couldn't find UITableViewCell for \(String(describing: name)), make sure the cell is registered with table view")
+        }
+        return cell
+    }
+}
