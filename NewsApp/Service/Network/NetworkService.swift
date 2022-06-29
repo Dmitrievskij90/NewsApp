7a858b3eb41bc18f67dd57921f4b80dbd7f93bf9
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2022-06-29 23:02:34 +0600

    add params for api keys to NetworkService

--- NewsApp/Service/Network/NetworkService.swift
+++ NewsApp/Service/Network/NetworkService.swift
@@ -8,42 +8,54 @@
 import Foundation
 
 class NetworkService: NetworkServiceProtocol {
+    // MARK: - API keys
+    private let newsAPIKey = "61bba430f9444209af20b7856ae3d12e"
+    private let finageAPIKey = "API_KEY57274AHAZDREZMQ1DZLXILFANMM0QH21"
+    private let twelvedataAPIKey = "d907af5f4dd84e25ad29b6387819d146"
+
+    // MARK: - fetch data functions
+
+    // func for search news by search term
     func searchNews(searchTerm: String, preferredCountry: String, completion: @escaping (NewsData?, Error?) -> Void) {
         if preferredCountry == "us" {
-            let urlString = "https://newsapi.org/v2/everything?q=\(searchTerm)&language=en&sortBy=publishedAt&pageSize=100&apiKey=61bba430f9444209af20b7856ae3d12e"
+            let urlString = "https://newsapi.org/v2/everything?q=\(searchTerm)&language=en&sortBy=publishedAt&pageSize=100&apiKey=\(newsAPIKey)"
             fetchData(with: urlString, completion: completion)
         }
 
-        let urlString = "https://newsapi.org/v2/everything?q=\(searchTerm)&language=\(preferredCountry)&sortBy=publishedAt&pageSize=100&apiKey=61bba430f9444209af20b7856ae3d12e"
+        let urlString = "https://newsapi.org/v2/everything?q=\(searchTerm)&language=\(preferredCountry)&sortBy=publishedAt&pageSize=100&apiKey=\(newsAPIKey)"
 
         fetchData(with: urlString, completion: completion)
     }
 
+    // func for fetch today news
     func fetchTodayNews(preferredCountry: String, completion: @escaping (NewsData?, Error?) -> Void) {
-        let urlString = "https://newsapi.org/v2/top-headlines?country=\(preferredCountry)&pageSize=100&apiKey=61bba430f9444209af20b7856ae3d12e"
+        let urlString = "https://newsapi.org/v2/top-headlines?country=\(preferredCountry)&pageSize=100&apiKey=\(newsAPIKey)"
 
         fetchData(with: urlString, completion: completion)
     }
 
+    // func for fetch news by category
     func fetchCategoriesNews(preferredCountry: String, preferredCategoty: String, completion: @escaping (NewsData?, Error?) -> Void) {
-        let urlString = "https://newsapi.org/v2/top-headlines?country=\(preferredCountry)&category=\(preferredCategoty)&pageSize=100&apiKey=61bba430f9444209af20b7856ae3d12e"
+        let urlString = "https://newsapi.org/v2/top-headlines?country=\(preferredCountry)&category=\(preferredCategoty)&pageSize=100&apiKey=\(newsAPIKey)"
 
         fetchData(with: urlString, completion: completion)
     }
 
+    // func for fetch stock data on today screen
     func fetchStockData(searchedStockCompanies: String, completion: @escaping ([StockData]?, Error?) -> Void) {
-        let urlString = "https://api.finage.co.uk/last/trade/stocks?symbols=\(searchedStockCompanies)&apikey=API_KEY57274AHAZDREZMQ1DZLXILFANMM0QH21"
+        let urlString = "https://api.finage.co.uk/last/trade/stocks?symbols=\(searchedStockCompanies)&apikey=\(finageAPIKey)"
 
         fetchData(with: urlString, completion: completion)
     }
 
+    // func for fetch stock data on chart screen
     func fetchStockChartData(searchedStockCompany: String, completion: @escaping (StockHistoryData?, Error?) -> Void) {
-        let urlString = "https://api.twelvedata.com/time_series?symbol=\(searchedStockCompany)&interval=1day&apikey=d907af5f4dd84e25ad29b6387819d146"
+        let urlString = "https://api.twelvedata.com/time_series?symbol=\(searchedStockCompany)&interval=1day&apikey=\(twelvedataAPIKey)"
 
         fetchData(with: urlString, completion: completion)
     }
 
-    func fetchData<T: Codable>(with urlString: String, completion: @escaping (T?, Error?) -> Void) {
+    private func fetchData<T: Codable>(with urlString: String, completion: @escaping (T?, Error?) -> Void) {
         guard let url = URL(string: urlString) else {
             print("Can't load url")
             return
